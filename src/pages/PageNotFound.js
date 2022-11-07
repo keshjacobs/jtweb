f6a2c1d5f95128674e2233bad75070896f04422b
Abraham <brhmdeen@gmail.com>
2022-11-07 10:46:59 +0100

    pagenotfound

--- src/pages/PageNotFound.js
+++ src/pages/PageNotFound.js
@@ -1,26 +1,20 @@
 import React, { useEffect } from "react";
-import json from "../../Config";
+import json from "../Config";
 
 
 function PageNotFound() {
-    
     useEffect(() => {
-      const timeout = setTimeout(() => {
-        var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-    if (/android/i.test(userAgent)) {
-        window.location.replace(json.playstore);
-    }
-
-    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-        window.location.replace(json.appstore);
-    }
-
-      }, 3000);
-  
-      return () => clearTimeout(timeout);
+            const timeout = setTimeout(() => {
+            var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+            if (/android/i.test(userAgent)) {
+                window.location.replace(json.playstore);
+            }
+            if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+                window.location.replace(json.appstore);
+            }
+            }, 3000);
+            return () => clearTimeout(timeout);
     }, []);
-  
     return <>Will redirect in 3 seconds...</>;
   }
 
